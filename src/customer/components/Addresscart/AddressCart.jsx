ec9a316c431818ca9d46fb883461c1f7a536c17e
akashkumarsky <akash220701@gmail.com>
2025-01-14 13:43:37 +0530

    AddressCart detail added

---
+++ src/customer/components/Addresscart/AddressCart.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Box, Button, Typography, Card, CardContent, CardActions, Divider } from '@mui/material';
+
+const AddressCart = () => {
+  const addresses = [
+    {
+      id: 1,
+      name: 'Akash kumar',
+      addressLine1: '2nd main road up 2 cross',
+      addressLine2: 'Sri sai teja Man PG',
+      city: 'Bengaluru',
+      state: 'karnataka',
+      zip: '56037',
+      phone: '7004972052',
+    }
+  ];
+
+  const handleSelect = (id) => {
+    console.log(`Address with ID ${id} selected.`);
+  };
+
+  const handleEdit = (id) => {
+    console.log(`Edit address with ID ${id}`);
+  };
+
+  return (
+    <div className=''>
+            <Box>
+                
+      {addresses.map((address) => (
+        
+        <Card key={address.id} sx={{ mb: 2  }}>
+            
+          <CardContent className='bg-gradient-to-r from-gray-800 via-gray-700 to-gray-900 text-white'>
+            <Typography variant="h6">{address.name}</Typography>
+            <Typography variant="body2">
+              {address.addressLine1}
+              {address.addressLine2 && `, ${address.addressLine2}`}
+            </Typography>
+            <Typography variant="body2">
+              {address.city}, {address.state} {address.zip}
+            </Typography>
+            <Typography variant="body2">Phone: {address.phone}</Typography>
+          </CardContent>
+          <Divider />
+  
+
+        </Card>
+      ))}
+    </Box>
+    </div>
+
+  );
+};
+
+export default AddressCart;
