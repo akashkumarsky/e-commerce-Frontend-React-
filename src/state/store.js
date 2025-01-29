f5e2ba2e349bb975d75e16f063c87428623117f1
akashkumarsky <akash220701@gmail.com>
2025-01-29 21:01:32 +0530

    add product and cart reducer

--- src/state/store.js
+++ src/state/store.js
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { thunk } from 'redux-thunk';
 import authReducer from './Auth/Reducer';
+import costmerProductsReducer from './Product/Reducer';
+import cartReducer from './Cart/Reducer';
 
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer
+    auth: authReducer,
+    product: costmerProductsReducer,
+    cart: cartReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk)
 });
