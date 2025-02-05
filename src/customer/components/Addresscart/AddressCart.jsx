f267d5126fe3e0f9b09e54de1bc1b5281859eefa
akashkumarsky <akash220701@gmail.com>
2025-02-05 18:38:36 +0530

    smoothing the design

--- src/customer/components/Addresscart/AddressCart.jsx
+++ src/customer/components/Addresscart/AddressCart.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import React from "react";
 
-
-const AddressCard = ({address}) => {
+const AddressCard = ({ address }) => {
   return (
-    <div>
-      {/* <h1 className="text-lg font-semibold py-4">Delivery Adress</h1> */}
-      <div className="space-y-3">
-        <p className="font-semibold">{`${address?.firstName} ${address?.lastName}`}</p>
+    <div className="p-4 border rounded-lg shadow-md bg-gray-700 ">
 
-        <p>
-          {`${address?.streetAddress} ${address?.city} ${address?.state} ${address?.zipCode}`}
+      <div className="mt-3 space-y-2 text-gray-700">
+        <p className="font-semibold text-white ">
+          {address?.firstName || "John"} {address?.lastName || "Doe"}
         </p>
 
-        <div className="space-y-1">
-          <p className="font-semibold">Phone Number</p>
-          <p>{address?.mobile}</p>
+        <p className="text-sm text-white opacity-70">
+          {address?.streetAddress || "123 Street Name"}, {address?.city || "City"},{" "}
+          {address?.state || "State"} - {address?.zipCode || "000000"}
+        </p>
+
+        <div className="space-y-1 text-white opacity-80">
+          <p className="font-semibold text-white opacity-80">Phone Number</p>
+          <p className="text-sm">{address?.mobile || "9876543210"}</p>
         </div>
       </div>
     </div>
